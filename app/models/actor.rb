40a3544994affb54d48b86c1030dcbcd2804a35e
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-10 23:55:30 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    list = []
+    Character.where(actor: self).each do |character|
+      list << "#{character.name} - #{character.show.name}"
+    end
+    list
+  end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    list = []
+    Character.where(show: self).each do |character|
+      list << character.actor.full_name
+    end
+    list
+  end
   
 end
